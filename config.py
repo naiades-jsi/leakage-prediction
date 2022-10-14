556ec12e6c6aa622303c3a23a5f1b12d1241d09e
Klemen Kenda <klemen.kenda@ijs.si>
2022-10-14 14:57:32 +0200

    Local changes

--- config.py
+++ config.py
@@ -1,8 +1,8 @@
 begin = True
-kafka_bootstrap_server = "188.166.121.54:9092"
+kafka_bootstrap_server = "localhost:9092"
 layout_DIR = "./layout/"
 layout_file_path = "./layout/clean_network.json"
-noise_data_DIR = "../dump/" 
+noise_data_DIR = "../dump/"
 kafka_params = {"bootstrap_servers": [kafka_bootstrap_server],
                 "auto_offset_reset": "latest",
                 "enable_auto_commit": "True",
